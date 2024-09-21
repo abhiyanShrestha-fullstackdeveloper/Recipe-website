c4411fa109f5fd2c64d5df0a1703e2869bce1846
abhiyanShrestha-fullstackdeveloper <abhiyanshrestha328@gmail.com>
2024-09-21 13:30:17 +0545

    Update models.py

--- kitchen_magic/recipe/models.py
+++ kitchen_magic/recipe/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Recipe(models.Model):
-    picture = models.ImageField(upload_to='')
+    picture = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     description = models.TextField()
